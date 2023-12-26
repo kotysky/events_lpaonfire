2c33bf68f2c80cc6b90b020908a6c8e51515ae76
JJ Huertas <kotysky@Belcebu>
2023-12-26 16:47:34 +0000

    2

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="LPAonFire logo"
+          />
+        </Link>
+
+        <div className="flex w-32 justify-end gap-3">
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;

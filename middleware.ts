e2e1d501f9d9e15f73d7ca363fe41b8b00679570
kotysky <kotysky@DESKTOP-IK71H8Q>
2024-01-07 08:51:48 +0000

    a ver

--- middleware.ts
+++ middleware.ts
@@ -4,12 +4,12 @@ export default authMiddleware({
   publicRoutes: [
     "/",
     "/events/:id",
-    "/api/webhook/clerck",
+    "/api/webhook/clerk",
     "/api/webhook/stripe",
     "/api/uploadthing",
   ],
   ignoredRoutes: [
-    "/api/webhook/clerck",
+    "/api/webhook/clerk",
     "/api/webhook/stripe",
     "/api/uploadthing",
   ],

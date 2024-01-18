58840bfe2f4f49fc248c200401c920d95a7c03d3
kotysky <kotysky@DESKTOP-IK71H8Q>
2024-01-18 20:41:39 +0000

    5:20

---
+++ components/shared/CategoryFilter.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const CategoryFilter = () => {
+  const [categories, setCategories] = useState("");
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      let newUrl = "";
+      if (categories) {
+        newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "query",
+          value: categories,
+        });
+      } else {
+        newUrl = removeKeysFromQuery({
+          params: searchParams.toString(),
+          keysToRemove: ["query"],
+        });
+      }
+      router.push(newUrl, { scroll: false });
+    }, 300);
+    return () => clearTimeout(delayDebounceFn);
+  }, [categories, searchParams, router]);
+
+  const onSelectCategory = (category: string) => {};
+
+  return (
+    <Select onValueChange={(value: string) => onSelectCategory(value)}>
+      <SelectTrigger className="select-field">
+        <SelectValue placeholder="Categoría" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="light">Light</SelectItem>
+        <SelectItem value="dark">Dark</SelectItem>
+        <SelectItem value="system">System</SelectItem>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default CategoryFilter;
--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -7,7 +7,7 @@ import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
 import { useRouter, useSearchParams } from "next/navigation";
 import { RouteMatcher } from "next/dist/server/future/route-matchers/route-matcher";
 
-const Search = () => {
+const Search = ({ placeholder = "Bucar título..." }: { placeholder?: string }) => {
   const [query, setQuery] = useState("");
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -37,7 +37,7 @@ const Search = () => {
       <Image src="/assets/icons/search.svg" alt="search" width={24} height={24} />
       <Input
         type="text"
-        placeholder="Buscar"
+        placeholder={placeholder}
         onChange={(e) => setQuery(e.target.value)}
         className="p-regular-16  border-0 bg-grey-50 outline-offset-0 placeholder-grey-500 focus:border-0 focus-visible-:ring-0 focus-visible:ring-offset-0"
       />
